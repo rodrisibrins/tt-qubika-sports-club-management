be4df9953863bfb7368bb864bfc12f49aafe07ec
Radium Rocket 32 <radiumrocket32@Radiums-MacBook-Pro-3.local>
2024-01-30 01:21:53 -0300

    tt-qubika-scm-03: add user login & create category flow to e2e

--- tests/createCategory.spec.ts
+++ tests/createCategory.spec.ts
@@ -1,22 +1,6 @@
 import { test, expect, request } from '@playwright/test';
 
 let randomEmail : string;
-// test('has title', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-
-//   // Expect a title "to contain" a substring.
-//   await expect(page).toHaveTitle(/Playwright/);
-// });
-
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
 
 function generateRandomEmail(): string {
   const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
@@ -32,7 +16,7 @@ function generateRandomEmail(): string {
   return email;
 }
 
-test.describe('create new user through API', () => {
+test.describe('create new user through API & create new category', () => {
   test.beforeAll(async ({ }) => {
     randomEmail = generateRandomEmail();
   });
@@ -47,8 +31,75 @@ test.describe('create new user through API', () => {
         ]
     }
     })
-    const responseBody = await response.json()
-    console.log(responseBody)
-  });
 
+    const responseBody = await response.json()
+    const userEmail = responseBody.email
+    const userPassword = "12345678"
+
+    console.log("User email:", userEmail)
+
+
+    await test.step('navigate to qubika club website', async () => {
+      await page.goto('https://club-administration.qa.qubika.com/#/auth/login');
+      await expect(page).toHaveURL(/login/);
+      await expect(page).toHaveTitle(/Qubika Club/);
+      await expect(page.locator('small')).toHaveText('Por favor ingrese correo y contraseña');
+      await expect(page.locator('.ni-lock-circle-open')).toBeInViewport();
+      await expect(page.locator('.ni-email-83')).toBeInViewport();
+    });
+
+    await test.step('login with the new user', async () => {
+      await page.locator('input[placeholder="Usuario o correo electrónico"]').fill(userEmail);
+      await page.locator('input[placeholder="Contraseña"]').fill(userPassword);
+      await page.locator(':text-is("Autenticar")').click();
+    });
+
+    await test.step('check user is navigated to dashboard page', async () => {
+      await page.waitForTimeout(1000);
+      await expect(page.locator('.navbar-brand')).toBeInViewport();
+      await page.waitForURL('https://club-administration.qa.qubika.com/#/dashboard');
+      await expect(page).toHaveURL(/dashboard/);
+    });
+
+    await test.step('navigate to category page', async () => {
+      await page.locator(':text-is(" Tipos de Categorias ")').click()
+      await page.waitForURL('https://club-administration.qa.qubika.com/#/category-type');
+      await expect(page).toHaveURL(/category-type/);
+      await expect(page.locator(':text-is("Tipos de categorías")')).toBeInViewport();
+    });
+
+    await test.step('create a new category', async () => {
+      await page.locator(':text-is("Adicionar")').click()
+      await expect(page.locator(':text-is("Adicionar tipo de categoría")')).toBeInViewport();
+      await page.locator('input[placeholder="Nombre de categoría"]').fill("Test Category");
+      await page.locator(':text-is("Aceptar")').click()
+      await expect(page.locator('#toast-container')).toBeInViewport();
+      await expect(page.locator('#toast-container')).toHaveText("Tipo de categoría adicionada satisfactoriamente");
+    });
+
+    await test.step('check Test Category was created', async () => {
+      await page.locator('.card-footer ul li:nth-last-child(2)').click()
+      await expect(page.locator('tbody tr:last-child td:first-child')).toHaveText("Test Category");
+      await expect(page.locator('tbody tr:last-child td:nth-last-child(2)')).toBeEmpty();
+    });
+
+    await test.step('create sub category', async () => {
+      await page.locator(':text-is("Adicionar")').click()
+      await expect(page.locator(':text-is("Adicionar tipo de categoría")')).toBeInViewport();
+      await page.locator('input[placeholder="Nombre de categoría"]').fill("Child Test Category");
+      await page.getByRole('checkbox', {name: "Es subcategoria?"}).click({force: true})
+      await page.locator('.ng-arrow-wrapper:last-child').click();
+      await page.getByLabel('Options list').locator('div').filter({ hasText: 'Test Category' }).last().click()
+      await page.locator(':text-is("Aceptar")').click()
+      await expect(page.locator('#toast-container')).toBeInViewport();
+      await expect(page.locator('#toast-container')).toHaveText("Tipo de categoría adicionada satisfactoriamente");
+    });
+
+    await test.step('check Child Test Category was created', async () => {
+      await page.locator('.card-footer ul li:nth-last-child(2)').click()
+      await expect(page.locator('tbody tr:last-child td:first-child')).toHaveText("Child Test Category");
+      await expect(page.locator('tbody tr:last-child td:nth-last-child(2)')).toHaveText("Test Category");
+    });
+
+  });
 });
